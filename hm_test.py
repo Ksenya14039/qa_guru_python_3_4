a0f5c73a73f32d4f59166b8bdf1131e341beaea3
ksenya14039 <ksenya14039@mail.ru>
2022-12-01 18:37:54 +0300

    first commit

---
+++ hm_test.py
@@ -0,0 +1,19 @@
+def name_function(func, *args):
+    name = func.__name__.title().replace('_', ' ')
+    print(name, *args)
+
+
+def open_browser(browser_name):
+    name_function(open_browser, browser_name)
+
+
+def go_to_companyname_homepage(page_url):
+    name_function(go_to_companyname_homepage, page_url)
+
+
+def find_registration_button_on_login_page(page_url, button_text):
+    name_function(find_registration_button_on_login_page, page_url, button_text)
+
+open_browser(browser_name='Chrome')
+go_to_companyname_homepage(page_url='yandex.ru')
+find_registration_button_on_login_page(page_url='yandex.ru', button_text='login')
